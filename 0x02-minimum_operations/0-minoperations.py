2180b240c0a89ab1f115648ee833032033f929f8
pendoagnes222 <pendoagnes22@gmail.com>
2023-01-12 21:45:57 +0300

    mimimum operators

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+""" In a text file, there is a single character H.
+    Your text editor can execute only two operations in this file:
+    Copy All and Paste. Given a number n what is Minimum Operations
+    to get n characters
+"""
 
 
 def minOperations(n):
+    """
+    a function that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file
+    args:
+        n: NUmber of charters to be displayed
+    return:
+          number of minimum operation
+    """
     curr = 1
     prev = 0
     count = 0
