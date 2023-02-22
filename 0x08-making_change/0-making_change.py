0d26aaf4372706dabb19542bc97d61705bc7fa44
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2023-02-22 07:06:22 +0300

    0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
+'''Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet
+    a given amount total.
+'''
+import sys
 
-"""
-fewest number of coins needed to meet total    
-"""
 
 def makeChange(coins, total):
-    """
-    Given a pile of coins of different values, determine the fewest
-    number of coins needed to meet a given amount total.
-        Return: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
-"""
-    sortArr = sorted(coins)
-    reversedCoin = sortArr[::-1]
-    ans = 0
-    for i in reversedCoin:
-        if total > i:
-            result = int(total / i)
-            total = total % i
-            ans = ans + result
-    if total == 0:
-        return(ans)
-    else:
-        return(-1)
+    '''
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    '''
+    if total <= 0:
+        return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
+
+    if table[total] == sys.maxsize:
+        return -1
+    return table[total]
